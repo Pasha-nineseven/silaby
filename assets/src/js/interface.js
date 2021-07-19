f5f0546bdcae9a16ec725ce2e52ae6e9566b839f
Want2stayHere <want2stayhere@gmail.com>
2021-07-19 14:18:10 +0300

    add

--- assets/src/js/interface.js
+++ assets/src/js/interface.js
@@ -2,3 +2,13 @@ const button = document.querySelector('.sidebar-toggle');
 button.addEventListener('click', e => {
   	document.querySelector('.sidebar').classList.toggle('collapsed')
 })
+
+//TABLE HIGHTLIGHT
+const table = document.querySelector('.table-users');
+table.addEventListener('click', function(e) {
+	var e = e || event,
+	el = e.srcElement || e.target;
+	if (el.tagName == 'INPUT') {
+		el.parentNode.parentNode.className = el.checked ? 'active' : '';
+	}
+})
