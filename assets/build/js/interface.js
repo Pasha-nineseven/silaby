5737a487b4f0498c7910ff20e92394284ba2ddb5
Want2stayHere <want2stayhere@gmail.com>
2021-07-20 19:12:52 +0300

    control

--- assets/build/js/interface.js
+++ assets/build/js/interface.js
@@ -1,14 +1,19 @@
-const button = document.querySelector('.sidebar-toggle');
-button.addEventListener('click', e => {
-  	document.querySelector('.sidebar').classList.toggle('collapsed')
-})
+document.addEventListener('DOMContentLoaded', () =>{
 
-//TABLE HIGHTLIGHT
-const table = document.querySelector('.table-users');
-table.addEventListener('click', function(e) {
-	var e = e || event,
-	el = e.srcElement || e.target;
-	if (el.tagName == 'INPUT') {
-		el.parentNode.parentNode.className = el.checked ? 'active' : '';
+	const button = document.querySelector('.sidebar-toggle');
+	button.addEventListener('click', e => {
+		document.querySelector('.sidebar').classList.toggle('collapsed')
+	})
+
+	//TABLE HIGHTLIGHT
+	const table = document.querySelector('.table-users');
+	if(!!table){
+		table.addEventListener('click', function(e) {
+			var e = e || event,
+			el = e.srcElement || e.target;
+			if (el.tagName == 'INPUT') {
+				el.parentNode.parentNode.className = el.checked ? 'active' : '';
+			}
+		})
 	}
 })
